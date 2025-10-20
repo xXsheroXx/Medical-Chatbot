dec4f08cea786840c21ca32365727635a7375a81
xXsheroXx <shero010807@gmail.com>
2025-10-20 15:43:32 +0200

    requirements are installed after creating a new environment called medibot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shirvan Hasan",
+    author_email="shero010807@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
